40ab793096f5327e455d62f70c9d2dfc55d497a2
amdYsr <ahmed.yassertaha@gmail.com>
2017-06-27 21:18:19 +0200

    mobile

--- js/plugins.js
+++ js/plugins.js
@@ -5,26 +5,30 @@ $(function(){
   var img = 1,
       body = $('body'),
       changeBG = setInterval(function(){
-                  setTimeout(showIntro, 1000);
+                  showIntro();
                   body.css({
                     backgroundImage: 'url(images/home-'+ img +'.jpg)',
                     backgroundSize: 'cover'
                   });
                   img++;
                   if(img > 6){img = 1}
-                }, 8000);
+                }, 9000);
   var intro = $('.intro');
   intro.css({
     paddingTop: (scrn-intro.height())*0.7,
     paddingBottom: (scrn-intro.height())*0.3
   });
-  showIntro();
+  intro.animate({
+    paddingTop: (scrn-intro.height())*0.5,
+    paddingBottom: (scrn-intro.height())*0.5,
+    opacity: 1
+  },500);
   function showIntro(){
-    intro.animate({
+    intro.delay(1000).animate({
       paddingTop: (scrn-intro.height())*0.5,
       paddingBottom: (scrn-intro.height())*0.5,
       opacity: 1
-    },500).delay(6000).animate({
+    },500).delay(5500).animate({
         opacity: 0
     },1000, function(){
       intro.css({
@@ -33,7 +37,15 @@ $(function(){
       });
     });
   }
+//================ mob-nav =========================
+  var menuBars = $('.fa-bars'),
+      menuList = $('.mob-nav ul'),
+      mobMenu = $('.mob-nav');
 
+  menuBars.on('click', function(){
+    intro.toggleClass('blur');
+    menuList.slideToggle(300);
+  })
 
 
 
